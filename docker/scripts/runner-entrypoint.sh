a92190edc49ddc7b040aab216666701d89a6f0fc
NeoGrAIph <NeoGrAIph@gmail.com>
2025-09-28 22:51:06 +0800

    Force-disable HTTP/3 via runner entrypoint

--- docker/scripts/runner-entrypoint.sh
+++ docker/scripts/runner-entrypoint.sh
@@ -8,6 +8,22 @@ cleanup() {
 }
 trap cleanup EXIT
 
+normalize_bool() {
+  case "${1:-}" in
+    1|true|TRUE|True|yes|YES|on|ON)
+      return 0
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+}
+
+if normalize_bool "${RUNNER_DISABLE_HTTP3:-}"; then
+  export MOZ_DISABLE_HTTP3=1
+  export MOZ_DISABLE_QUIC=1
+fi
+
 if [ "${RUNNER_VNC_LEGACY:-0}" = "1" ]; then
   /usr/local/bin/vnc-start.sh &
   VNC_PID=$!
